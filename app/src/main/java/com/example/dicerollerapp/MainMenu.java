9c6c8bfa6057c06da1d01b12ecd4f774c52c4b9b
Arthur Maia <arthurmaiav@gmail.com>
2020-03-19 17:09:14 -0300

    Novo layout

--- app/src/main/java/com/example/dicerollerapp/MainMenu.java
+++ app/src/main/java/com/example/dicerollerapp/MainMenu.java
@@ -12,11 +12,7 @@ import com.example.dicerollerapp.MenuItem.ItemMenu;
 import com.example.dicerollerapp.MenuItem.RVItemMenuAdapter;
 import com.example.dicerollerapp.Utilities.ScoreCounter.ScoreCounter;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.ArrayList;
 import java.util.List;
