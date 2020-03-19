e65a77a0bd0187dcec51a9f1f9e8776a3480c8a1
Arthur Maia <arthurmaiav@gmail.com>
2020-03-18 22:23:06 -0300

    Ad Banner adicionado no menu principal

--- app/src/main/java/com/example/dicerollerapp/MainMenu.java
+++ app/src/main/java/com/example/dicerollerapp/MainMenu.java
@@ -11,6 +11,12 @@ import com.example.dicerollerapp.Utilities.DiceRoller.DiceRoller;
 import com.example.dicerollerapp.MenuItem.ItemMenu;
 import com.example.dicerollerapp.MenuItem.RVItemMenuAdapter;
 import com.example.dicerollerapp.Utilities.ScoreCounter.ScoreCounter;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +36,13 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        adView.loadAd(adRequest);
 
         initializeData();
 
